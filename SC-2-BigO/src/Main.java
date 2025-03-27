3a17909885bc6903275cda70eee220c8c66ecfbe
André Torres <150715327+andtors@users.noreply.github.com>
2025-03-27 16:22:28 -0300

    SC-2-Inicio

---
+++ SC-2-BigO/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    /*
+    BigO - Intro
+
+    Como definir se dois códigos que fazem a mesma coisa, porém é escrito de formas diferentes?
+    BigO é uma forma de comparar de forma matematica qual código é mais eficiente de duas formas:
+    Time Complexity irá verificar quanto tempo demora mas principalmente quantas "ordens" precisaram ser executadas.
+    Space Complexity irá ver quanto de espaço o código ocupa e sua eficiencia.
+
+
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
